b5baffa9e66fe8c6dbac419edebadc5e3ac9f66d
Neil Day <neil@ashfordweb.co.uk>
2023-05-17 01:53:13 +0100

    Add NEXTAUTH

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = {
     target: "experimental-serverless-trace",
     env: {
       NEXT_PUBLIC_WP_API_URL: process.env.NEXT_PUBLIC_WP_API_URL,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
       WP_JWT_AUTH_SECRET_KEY: process.env.WP_JWT_AUTH_SECRET_KEY,
       NEXTAUTH_SECRET_KEY: process.env.NEXTAUTH_SECRET_KEY,
       NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
