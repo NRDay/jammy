61d81ae52ec093f315c12157fbcd59362152ce81
Neil Day <neil@ashfordweb.co.uk>
2023-05-17 01:29:10 +0100

    Add env vars

--- next.config.js
+++ next.config.js
@@ -2,5 +2,12 @@ module.exports = {
     // Target must be experimental-serverless-trace
     // Your build time will be longer with this option
     target: "experimental-serverless-trace",
+    env: {
+      NEXT_PUBLIC_WP_API_URL: process.env.NEXT_PUBLIC_WP_API_URL,
+      WP_JWT_AUTH_SECRET_KEY: process.env.WP_JWT_AUTH_SECRET_KEY,
+      NEXTAUTH_SECRET_KEY: process.env.NEXTAUTH_SECRET_KEY,
+      NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+      STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    }
   };
 
